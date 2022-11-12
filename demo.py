b018bcfb7e2f30dbdd8c91c305f0119b9e59ab0a
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-13 00:15:35 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+# Division of two numbers
+#take two user input number and divide them
+num1=int(input("enter 1st integer no."))
+num2=int(input("enter 2nd integer no."))
+print("the Division of",num1 ,"and",num2 ,"are",num1/num2)
